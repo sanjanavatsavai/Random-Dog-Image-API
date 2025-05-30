2a39eabc7b2cd159b6ae2e2b57cd7f43079556cb
sanjanavatsavai <sanju82@outlook.com>
2025-05-30 16:27:14 -0400

    added breed names

--- random-dog-image/src/DogApp.js
+++ random-dog-image/src/DogApp.js
@@ -1,15 +1,22 @@
 import React, {useState, useEffect} from 'react'
+import './DogApp.css';
 
 
 function DogApp() {
     const [dogImage, setDogImage] = useState('');
     const [loading, setLoading] = useState(true);
+    const [breed, setBreed] = useState('');
 
     const fetchDogImage = async () => {
         setLoading(true);
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
         const data = await response.json();
         setDogImage(data.message);
+
+        const parts = data.message.split('/');
+        const breedName = parts[parts.indexOf('breeds') + 1];
+        setBreed(breedName.replace('-', ' '));
+
         setLoading(false);
     };
 
@@ -18,13 +25,19 @@ function DogApp() {
     },[]);
 
     return (
-    <div className="dog-container">
+     <div className="dog-container">
       <h1>🐶 Random Dog App</h1>
-      {loading ? <p>Loading...</p> : <img src={dogImage} alt="Random Dog" className="dog-image" />}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <img src={dogImage} alt="Random Dog" className="dog-image" />
+          <p className="breed-name">Breed: {breed}</p>
+        </>
+      )}
       <button onClick={fetchDogImage}>Get Another Dog</button>
     </div>
   );
-  
 }
 
 
